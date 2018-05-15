0ce49ab8984cbb8bcfe5b93410cb67f716b257f5
uitlaber <uitlaber@gmail.com>
2018-05-15 09:02:04 +0600

    Added settings page

--- Plugin.php
+++ Plugin.php
@@ -6,6 +6,7 @@ use System\Classes\PluginBase;
 use Event;
 use BackendMenu;
 use Backend;
+use Uit\Bmenu\Models\Settings;
 
 class Plugin extends PluginBase
 {
@@ -22,35 +23,39 @@ class Plugin extends PluginBase
         ]
     ];
 
+    public function registerSettings()
+    {
+        return [
+            'location' => [
+                'label'       => 'Backend menu in frontend',
+                'description' => 'Toggle backend menu in frontend',
+                'category'    => 'system::lang.system.categories.cms',
+                'icon'        => 'icon-bars',
+                'class'       => 'Uit\Bmenu\Models\Settings',
+                'url'         => Backend::url('uit/bmenu/settings'),
+                'order'       => 500,
+            ]
+        ];
+    }
 
     public function boot()
     {
 
-        Event::listen('cms.page.postprocess', function ($controller, $url, $page, $dataHolder) {
+        if(Settings::get('show_menu')) {
+            Event::listen('cms.page.postprocess', function ($controller, $url, $page, $dataHolder) {
 
-            $this->loadBackendUser();
-            if(!is_null($this->user)) {
-                $this->registerBackendNavigations();
-                $html = '';
-                $html .= $this->makeLayoutPartial(base_path('plugins/uit/bmenu/partials/menu'));
-                $html .= $this->makeAssets().'</body>';
-                $dataHolder->content = str_replace('</body>',$html, $dataHolder->content);
-            }
+                $this->loadBackendUser();
+                if (!is_null($this->user)) {
+                    $this->registerBackendNavigations();
+                    $html = '';
+                    $html .= $this->makeLayoutPartial(base_path('plugins/uit/bmenu/partials/menu'));
+                    $html .= $this->makeAssets() . '</body>';
+                    $dataHolder->content = str_replace('</body>', $html, $dataHolder->content);
+                }
 
-        });
-
-    }
-
-    public function makeAssets()
-    {
-        $assetsHtml = '';
-        foreach ($this->assets['js'] as $js){
-            $assetsHtml .= '<script src="'.url($js).'"></script>';
+            });
         }
-        foreach ($this->assets['css'] as $css){
-            $assetsHtml .= '<link rel="stylesheet" href="'.url($css).'"/>';
-        }
-        return $assetsHtml;
+
     }
 
     public function loadBackendUser()
@@ -63,19 +68,19 @@ class Plugin extends PluginBase
         BackendMenu::registerCallback(function ($manager) {
             $manager->registerMenuItems('October.Backend', [
                 'dashboard' => [
-                    'label'       => 'backend::lang.dashboard.menu_label',
-                    'icon'        => 'icon-dashboard',
-                    'iconSvg'     => 'modules/backend/assets/images/dashboard-icon.svg',
-                    'url'         => Backend::url('backend'),
+                    'label' => 'backend::lang.dashboard.menu_label',
+                    'icon' => 'icon-dashboard',
+                    'iconSvg' => 'modules/backend/assets/images/dashboard-icon.svg',
+                    'url' => Backend::url('backend'),
                     'permissions' => ['backend.access_dashboard'],
-                    'order'       => 10
+                    'order' => 10
                 ],
                 'cms' => [
-                    'label'       => 'cms::lang.cms.menu_label',
-                    'icon'        => 'icon-magic',
-                    'iconSvg'     => 'modules/cms/assets/images/cms-icon.svg',
-                    'url'         => Backend::url('cms'),
-                    'order'       => 100,
+                    'label' => 'cms::lang.cms.menu_label',
+                    'icon' => 'icon-magic',
+                    'iconSvg' => 'modules/cms/assets/images/cms-icon.svg',
+                    'url' => Backend::url('cms'),
+                    'order' => 100,
                     'permissions' => [
                         'cms.manage_content',
                         'cms.manage_assets',
@@ -85,22 +90,34 @@ class Plugin extends PluginBase
                     ],
                 ],
                 'media' => [
-                    'label'       => 'backend::lang.media.menu_label',
-                    'icon'        => 'icon-folder',
-                    'iconSvg'     => 'modules/backend/assets/images/media-icon.svg',
-                    'url'         => Backend::url('backend/media'),
+                    'label' => 'backend::lang.media.menu_label',
+                    'icon' => 'icon-folder',
+                    'iconSvg' => 'modules/backend/assets/images/media-icon.svg',
+                    'url' => Backend::url('backend/media'),
                     'permissions' => ['media.*'],
-                    'order'       => 200
-                ],'system' => [
-                    'label'       => 'system::lang.settings.menu_label',
-                    'icon'        => 'icon-cog',
-                    'iconSvg'     => 'modules/system/assets/images/cog-icon.svg',
-                    'url'         => Backend::url('system/settings'),
+                    'order' => 200
+                ], 'system' => [
+                    'label' => 'system::lang.settings.menu_label',
+                    'icon' => 'icon-cog',
+                    'iconSvg' => 'modules/system/assets/images/cog-icon.svg',
+                    'url' => Backend::url('system/settings'),
                     'permissions' => [],
-                    'order'       => 1000
+                    'order' => 1000
                 ]
             ]);
         });
 
+    }
+
+    public function makeAssets()
+    {
+        $assetsHtml = '';
+        foreach ($this->assets['js'] as $js) {
+            $assetsHtml .= '<script src="' . url($js) . '"></script>';
+        }
+        foreach ($this->assets['css'] as $css) {
+            $assetsHtml .= '<link rel="stylesheet" href="' . url($css) . '"/>';
+        }
+        return $assetsHtml;
     }
 }
